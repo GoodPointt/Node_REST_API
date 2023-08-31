32e09f6e17b40cf1b0056c8243d0c944753da91b
Dmytro Petyshyn <dev6012@gmail.com>
2023-08-31 16:39:57 +0300

    configure CRUD to work with db-contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,17 +3,28 @@ const express = require('express');
 const ctrl = require('../../controllers/contacts');
 const router = express.Router();
 
-const { validateBody } = require('../../middlewares');
-const schemas = require('../../schemas/contacts');
+const { validateBody, isValidId } = require('../../middlewares');
+const { schemas } = require('../../models/contact');
 
 router.get('/', ctrl.listAll);
 
-router.get('/:contactId', ctrl.getById);
+router.get('/:contactId', isValidId, ctrl.getById);
 
-router.post('/', validateBody(schemas.addSchema), ctrl.addNew);
+router.post('/', validateBody(schemas.contactJoiSchema), ctrl.addNew);
 
-router.put('/:contactId', validateBody(schemas.addSchema), ctrl.updById);
+router.put(
+  '/:contactId',
+  isValidId,
+  validateBody(schemas.contactJoiSchema),
+  ctrl.updById
+);
+router.patch(
+  '/:contactId/favorite',
+  isValidId,
+  validateBody(schemas.updateFavoriteJoiSchema),
+  ctrl.updFavorite
+);
 
-router.delete('/:contactId', ctrl.deleteById);
+router.delete('/:contactId', isValidId, ctrl.deleteById);
 
 module.exports = router;
